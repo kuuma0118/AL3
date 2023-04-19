a6eeb1bd2e86482e3935b7fe13440febea7afb20
k022g0093@g.neec.ac.jp <k022g0093@g.neec.ac.jp>
2023-04-19 16:23:56 +0900

    AL3_01_02

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,11 +8,21 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
 /// </summary>
 class GameScene {
+
+	// 3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+	//デバックカメラ
+	DebugCamera* debugCamera_ = nullptr;
 
 public: // メンバ関数
 	/// <summary>
@@ -41,6 +51,17 @@ public: // メンバ関数
 	void Draw();
 
 private: // メンバ変数
+	// 音声再生ハンドル
+	uint32_t voiceHandle_ = 0;
+    // サウンドデータハンドル
+	uint32_t soundDataHandle_ = 0;
+	// 　スプライト
+	Sprite* sprite_ = nullptr;
+	// 　テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	// ImGuiで値を入力する変数
+	float inputFloat3[3] = {0, 0, 0};
+
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
