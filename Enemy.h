19749c05cea346295903a67b8712b231ea85a3bd
k022g0093@g.neec.ac.jp <k022g0093@g.neec.ac.jp>
2023-06-09 14:39:56 +0900

    02_07

--- Enemy.h
+++ Enemy.h
@@ -4,14 +4,24 @@
 
 class Enemy {
 public:
-	void Initialize(Model* model, const Vector3& position);
+	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
 
 	void Update();
 
 	void Draw(const ViewProjection& viewProjection);
 
 private:
+	enum class Phase {
+		Approach,
+		Leave,
+	};
+
 	WorldTransform worldTransform_;
 	Model* model_ = nullptr;
 	uint32_t textureHandle_ = 0u;
+	Vector3 velocity_;
+	Phase phase_ = Phase::Approach;
+
+	void Approach();
+	void Leave();
 };
